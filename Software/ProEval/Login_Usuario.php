1d23f8501ca99fbb26be91239423c1e5419c310a
alejandra <ale.zamo_gutierrez@hotmail.com>
2017-10-06 01:05:47 -0500

    Contiene el login de adminidtrador y usiar

--- Software/ProEval/Login_Usuario.php
+++ Software/ProEval/Login_Usuario.php
@@ -9,32 +9,27 @@
 
 		$nombre=$_GET["txtnombre"];
 		$contrasena=$_GET["txtcontrasena"];
-		$conexion=mysqli_connect("localhost","root","","filmaciones");
-		$consulta=mysqli_query($conexion,"select * from usuario where Nombre='$nombre' and contrasena='$contrasena' and Tipo=1;");
+		$conexion=mysqli_connect("localhost","root","","delphi");
+		$consulta=mysqli_query($conexion,"select * from usuarios where Nombre='$nombre' and contrasena='$contrasena' and Tipo=2;");
 		$RESULTADO=mysqli_num_rows($consulta);
 		
-		if((mysqli_num_rows($consulta)>0) && ($nombre == "Administrador") && ($contrasena == "abc")){
+		if(mysqli_num_rows($consulta)>0){
 			?>
 			<script type="text/javascript">
 			
-				window.location.href="administrador_inicio.php?txtnombre=<?php echo $nombre; ?>";
-				
+				window.location.href="Inicio_Usuario.php?txtnombre=<?php echo $nombre; ?>";
 				//window.location.href="usuario_inicio.php?txtnombre=<?php// <input type="hidden" name="txtnombre" value//="<?php echo $nombre;?>"> echo $nombre; ?>";
 			</script>
-
 			<?php
 		}else{
-
 			?>
 			<script type="text/javascript">
-				alert("no se pudo iniciar");
-				window.location.href="administrador_login.html";
+				alert("No se puede iniciar, intenta de nuevo");
+				window.location.href="Login_Usuario.html";
 			</script>
 			<?php
-
 		}
-		 	mysqli_close($conexion);	
+		 	mysqli_close($conexion);
 		?>
-	
 	</body>
 </html>
